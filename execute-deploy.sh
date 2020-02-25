29816bc98876c475262681154beeff863ae65c89
devgaram <joingaram@gmail.com>
2020-02-26 02:18:21 +0900

    execute-deploy.sh 권한추가

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-/home/ec2-user/build/deploy.sh > /dev/null 2> /dev/null < /dev/null &
+cd /home/ec2-user/build
+chmod +x ./deploy.sh
+./deploy.sh > /dev/null 2> /dev/null < /dev/null &
