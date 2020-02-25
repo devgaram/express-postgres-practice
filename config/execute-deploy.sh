ae0a0d22190962dad501944fc87d465d7def0115
devgaram <joingaram@gmail.com>
2020-02-26 01:52:48 +0900

    경로 수정

--- config/execute-deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/home/ec2-user/build/config/deploy.sh > /dev/null 2> /dev/null < /dev/null &
---
+++ execute-deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/home/ec2-user/build/deploy.sh > /dev/null 2> /dev/null < /dev/null &
