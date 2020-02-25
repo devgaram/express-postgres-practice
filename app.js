9e06ed4cec76bad846a9a8e278f490f2ae0c3e3a
devgaram <joingaram@gmail.com>
2020-02-26 01:09:40 +0900

    commit

--- app.js
+++ app.js
@@ -34,7 +34,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 //app.use('/api', api);
 
 // catch 404 and forward to error handler
